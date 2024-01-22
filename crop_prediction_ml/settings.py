0b0600179fcdebadf6782f33dd055c388f210181
niranjankolpe <en20170973@git-india.edu.in>
2024-01-22 17:47:39 +0530

    Trial 2

--- crop_prediction_ml/settings.py
+++ crop_prediction_ml/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static_files'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static_files'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files')
--- predictor/urls.py
+++ predictor/urls.py
@@ -4,7 +4,6 @@ from predictor import views
 
 urlpatterns = [
     path('',     views.index, name="index"),
-    path('home', views.home,  name="home"),
     path('predictor', views.predictor, name="predictor"),
     path('predict_refresh', views.predict_refresh, name="predict_refresh"),
     path('predict', views.predict, name="predict"),
