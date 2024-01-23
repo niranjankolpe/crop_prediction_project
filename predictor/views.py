6490230e986a19f2d3eae4d6e45c56c163d0968a
niranjankolpe <en20170973@git-india.edu.in>
2024-01-23 19:53:58 +0530

    No libraries

--- predictor/views.py
+++ predictor/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 
 from django.conf import settings
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from  sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+# import pandas as pd
+# from sklearn.linear_model import LogisticRegression
+# from  sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
 import joblib
 
 def index(request):
@@ -15,16 +15,16 @@ def predictor(request):
     return render(request, "predictor/predictor.html", context)
 
 def predict_refresh(request):
-    df = pd.read_csv("media_files/cp.csv")
-    x = df.drop(columns=['label'], axis=1)
-    y = df['label']
-    sclr = StandardScaler()
-    x_scaled = sclr.fit_transform(x)
-    x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size = 0.2, random_state = 42)
-    log_reg = LogisticRegression()
-    log_reg.fit(x_train, y_train)
-    with open('media_files/crop_prediction_model', 'wb') as f:
-        joblib.dump(log_reg, f)
+    # df = pd.read_csv("media_files/cp.csv")
+    # x = df.drop(columns=['label'], axis=1)
+    # y = df['label']
+    # sclr = StandardScaler()
+    # x_scaled = sclr.fit_transform(x)
+    # x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size = 0.2, random_state = 42)
+    # log_reg = LogisticRegression()
+    # log_reg.fit(x_train, y_train)
+    # with open('media_files/crop_prediction_model', 'wb') as f:
+    #     joblib.dump(log_reg, f)
     return redirect("predictor")
     
 def predict(request):
@@ -32,18 +32,21 @@ def predict(request):
         return redirect("predictor")
 
     if request.method == 'POST':
-        N = int(request.POST['N'])
-        P = int(request.POST['P'])
-        K = int(request.POST['K'])
-        temperature = float(request.POST['temperature'])
-        humidity = float(request.POST['humidity'])
-        ph = float(request.POST['ph'])
-        rainfall = float(request.POST['rainfall'])
-        
-        model = joblib.load("media_files/crop_prediction_model")
+        predict_value = ""
 
-        predict_value = model.predict([[N, P, K, temperature, humidity, ph, rainfall]])
-        predict_value = str(predict_value[0])
+        # N = int(request.POST['N'])
+        # P = int(request.POST['P'])
+        # K = int(request.POST['K'])
+        # temperature = float(request.POST['temperature'])
+        # humidity = float(request.POST['humidity'])
+        # ph = float(request.POST['ph'])
+        # rainfall = float(request.POST['rainfall'])
+        
+        # model = joblib.load("media_files/crop_prediction_model")
+
+        # predict_value = model.predict([[N, P, K, temperature, humidity, ph, rainfall]])
+        # predict_value = str(predict_value[0])
+
 
         data = {'prediction':predict_value, 'media_url': settings.MEDIA_URL}
 
