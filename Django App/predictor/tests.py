116ef6734eaf55d9ac080d2f24eae99dc5f742da
Niranjan Kolpe <niranjankolpe@gmail.com>
2026-01-22 21:28:19 +0530

    Stabilized Version

--- Django App/predictor/tests.py
+++ Django App/predictor/tests.py
@@ -1,3 +1,11 @@
+# app/tests.py
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_home_status(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 302) # Response Code 302 == Redirect
+    
+    def test_prediction_home(self):
+        response = self.client.get("/predictor")
+        self.assertEqual(response.status_code, 200) # Respomse Code 200 == Success
