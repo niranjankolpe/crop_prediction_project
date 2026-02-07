4a70d5f52dd95fc04640de3b406f6b97e449e00c
Niranjan Kolpe <niranjankolpe@gmail.com>
2026-02-08 00:12:25 +0530

    Migrated Test Case for Analytics page to Analytics App's test.py

--- Django App/analytics/tests.py
+++ Django App/analytics/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SimpleTest(TestCase):
+    def test_analytics(self):
+        response = self.client.get("/analytics")
+        self.assertEqual(response.status_code, 302)
--- Django App/predictor/tests.py
+++ Django App/predictor/tests.py
@@ -17,10 +17,6 @@ class SimpleTest(TestCase):
     def test_donate(self):
         response = self.client.get("/donate")
         self.assertEqual(response.status_code, 200)
-
-    def test_analytics(self):
-        response = self.client.get("/analytics")
-        self.assertEqual(response.status_code, 302)
     
     def test_signup(self):
         response = self.client.get("/signup")
