7062052a6ab5d74c9ccfd1ebb9cc65d5ad0ae610
Niranjan Kolpe <niranjankolpe@gmail.com>
2026-03-06 23:48:02 +0530

    Minor update. Added a test case for model retrain view

--- Django App/analytics/tests.py
+++ Django App/analytics/tests.py
@@ -5,3 +5,7 @@ class SimpleTest(TestCase):
     def test_analytics(self):
         response = self.client.get("/analytics/")
         self.assertEqual(response.status_code, 302)
+    
+    def test_predict_refresh(self):
+        response = self.client.get("/analytics/predict_refresh")
+        self.assertEqual(response.status_code, 302)
